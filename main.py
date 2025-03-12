f76b29720edf2df5760070e34eacae23beb9d3a7
Kevin Veen-Birkenbach <kevin@veen.world>
2025-03-12 08:57:26 +0100

    Update main.py
    Added suffix parameter

--- main.py
+++ main.py
@@ -3,24 +3,37 @@
 import argparse
 import os
 
-def rename_files(search, replace, recursive=False):
+def rename_files(search, replace, recursive=False, suffix_only=False):
     if recursive:
-        # Walk through directories recursively
         for root, _, files in os.walk('.'):
             for filename in files:
-                if search in filename:
-                    newname = filename.replace(search, replace)
-                    oldpath = os.path.join(root, filename)
-                    newpath = os.path.join(root, newname)
-                    print(f"Renaming: '{oldpath}' -> '{newpath}'")
-                    os.rename(oldpath, newpath)
+                oldpath = os.path.join(root, filename)
+                if suffix_only:
+                    # Only rename if the file name ends with the search string
+                    if filename.endswith(search):
+                        newname = filename[:-len(search)] + replace
+                        newpath = os.path.join(root, newname)
+                        print(f"Renaming: '{oldpath}' -> '{newpath}'")
+                        os.rename(oldpath, newpath)
+                else:
+                    if search in filename:
+                        newname = filename.replace(search, replace)
+                        newpath = os.path.join(root, newname)
+                        print(f"Renaming: '{oldpath}' -> '{newpath}'")
+                        os.rename(oldpath, newpath)
     else:
-        # Rename files in the current directory only
         for filename in os.listdir('.'):
-            if os.path.isfile(filename) and search in filename:
-                newname = filename.replace(search, replace)
-                print(f"Renaming: '{filename}' -> '{newname}'")
-                os.rename(filename, newname)
+            if os.path.isfile(filename):
+                if suffix_only:
+                    if filename.endswith(search):
+                        newname = filename[:-len(search)] + replace
+                        print(f"Renaming: '{filename}' -> '{newname}'")
+                        os.rename(filename, newname)
+                else:
+                    if search in filename:
+                        newname = filename.replace(search, replace)
+                        print(f"Renaming: '{filename}' -> '{newname}'")
+                        os.rename(filename, newname)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -30,10 +43,11 @@ def main():
     parser.add_argument('replacement_string', type=str, help='The string to replace the search string with.')
     parser.add_argument('-r', '--recursive', action='store_true',
                         help='Perform renaming recursively in subdirectories.')
+    parser.add_argument('-s', '--suffix-only', action='store_true',
+                        help='Only rename the suffix (file extension) if the file name ends with the search string.')
     
     args = parser.parse_args()
-
-    rename_files(args.search_string, args.replacement_string, recursive=args.recursive)
+    rename_files(args.search_string, args.replacement_string, recursive=args.recursive, suffix_only=args.suffix_only)
 
 if __name__ == '__main__':
     main()
